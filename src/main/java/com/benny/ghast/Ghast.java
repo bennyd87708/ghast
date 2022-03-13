1afa8fc060a10b707962ef57107e00b8f8d02b06
Benjamin Feinberg <bennyd87708@gmail.com>
2022-03-12 20:29:26 -0800

    ghast

---
+++ src/main/java/com/benny/ghast/Ghast.java
@@ -0,0 +1,11 @@
+package com.benny.ghast;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ghast implements ModInitializer {
+    public static final String MOD_ID = "ghast";
+    @Override
+    public void onInitialize() {
+
+    }
+}
